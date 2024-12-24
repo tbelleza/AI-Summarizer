2a3144c1a1688668a64307781537875b15f70718
tbelleza <theresabelleza@gmail.com>
2024-12-24 11:08:16 -0800

    Hero section

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,8 +1,24 @@
-import React from 'react'
+import { logo } from '../assets'
 
 const Hero = () => {
   return (
-    <div>Hero</div>
+    <header className='w-full flex justify-center items-center flex-col'>
+      <nav className="flex justify-between items-center flex-row w-full mb-10 pt-3">
+        <img className='w-28 object-contain' src={logo} alt="sumz-logo" />
+
+        <button type='button' className='black_btn' onClick={() => window.open('https://github.com/tbelleza')}>
+          Github
+        </button>
+      </nav>
+
+      <h1 className="head_text">
+        Summarize Articles with <br className='max-md:hidden' />
+        <span className='orange_gradient'>OpenAI GPT-4</span>
+      </h1>
+      <h2 className="desc">
+        Simplify your reading with Summize, an article summarizer that transforms lengthy articles into clear and concise summaries
+      </h2>
+    </header>
   )
 }
 
